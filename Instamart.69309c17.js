4d6c374e0430531044e6764fe209188462155d06
Eswar3321 <eswararao8786@gmail.com>
2025-02-19 10:37:56 +0530

    Updates

---
+++ Instamart.69309c17.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("5KNkF",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),i=()=>(0,a.jsx)("h1",{className:"m-8 text-3xl",children:"Hey This is Instamart"})});
+//# sourceMappingURL=Instamart.69309c17.js.map
